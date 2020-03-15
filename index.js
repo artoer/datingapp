e183a7c61cfa524b04888e7edfa167d847b9c4b4
artoer <arthur_menken@hotmail.com>
2020-03-15 20:56:01 +0100

    Updating mongo
    Ik probeer er nu achter te komen hoe ik items kan verwijderen (de remove function uit de slides)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const slug = require('slug')
 const bodyParser = require('body-parser')
 const find = require('array-find')
@@ -8,27 +8,19 @@ const multer = require('multer')
 const upload = multer({
     dest: 'static/upload/'
 })
+const mongo = require('mongodb')
 
+require('dotenv').config()
 
-let data = [
-    {
-        id: 'vakantie-foto',
-        title: 'Ik in Griekenland',
-        photo: 'jorren.PNG',
-        description: 'Het was leuk'
-        }
-    ,
-    {
+var db = null
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
 
-        id: 'selfie',
-        title: 'Selfie!',
-        photo: 'paard.JPG',
-        description: 'Dit ben ik dus'
+mongo.MongoClient.connect(url, function (err, client) {
+    if (err) throw err
+    db = client.db(process.env.DB_NAME)
+})
 
-    }
-
-]
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`De app is nu online op http://localhost:${port}!`))
 
 app.use('/static', express.static('static'));
 
@@ -41,69 +33,79 @@ app.use(bodyParser.urlencoded({
 
 app.post('/', upload.single('photo'), add)
 app.get('/', pictures)
-app.post('/', add)
+app.delete('/', remove)
 app.get('/upload', form)
 app.get('/:id', picture)
-
-//app.get('/helloworld', (req, res) => res.render('helloworld.ejs', {
-//    name: 'Arthur'
-//}))
-//app.get('/about', (req, res) => res.send('Hello World!'))
-//app.get('/contact', (req, res) => res.sendfile('static/img/kat.png'))
 
 app.use(function (req, res, next) {
     res.status(404).send('404 Page Not Found!')
 })
 
 //Rendert list.ejs met alle data (alle foto's)
-function pictures(req, res) {
-    res.render('list.ejs', {
-        data: data
-    })
+function pictures(req, res, next) {
+    db.collection('picture').find().toArray(done)
+
+    function done(err, data) {
+        if (err) {
+            next(err)
+        } else {
+            res.render('list.ejs', {
+                data: data
+            })
+        }
+    }
 }
 
 function picture(req, res, next) {
     var id = req.params.id
-    var picture = find(data, function (value) {
-        return value.id === id
-    })
-    res.render('detail.ejs', {
-        data: picture
-    })
-}
-// Hierboven geprobeerd onderstaande code te laten werken met res.render, is niet gelukt
+    db.collection('picture').findOne({
+        _id: mongo.ObjectID(id)
+    }, done)
 
-// Deze code heb ik gevonden in oude slides, niet heel netjes maar het werkt wel
-//function picture(req, res) {
-//  var id = req.params.id
-//  var doc = '<!doctype html>'
-//  var picture = find(data, function (value) {
-//    return value.id === id
-//  })
-//
-//  doc += '<head><link rel="stylesheet" type="text/css" href="http://localhost:3000/static/css/styles.css">'
-//  doc += '<title>' + picture.title + ' - Mijn Gallerij</title></head>'
-//  doc += '<img alt src="/upload/<%= data.cover %>">'
-//  doc += '<h1>' + picture.title + '</h1>'
-//  doc += '<p>' + picture.description + '</p>'
-//
-//  res.send(doc)
-//}
-
-// Rendert uploadpagina
-function form(req, res) {
-    res.render('upload.ejs')
+    function done(err, data) {
+        if (err) {
+            next(err)
+        } else {
+            res.render('detail.ejs', {
+                data: data
+            })
+        }
+    }
 }
 
-function add(req, res) {
-    var id = slug(req.body.title).toLowerCase()
-
-    data.push({
-        id: id,
+function add(req, res, next) {
+    db.collection('picture').insertOne({
         title: req.body.title,
         photo: req.file ? req.file.filename : null,
         description: req.body.description
-    })
+    }, done)
 
-    res.redirect('/' + id)
+    function done(err, data) {
+        if (err) {
+            next(err)
+        } else {
+            res.redirect('/' + data.insertedId)
+        }
+    }
+}
+
+function remove(req, res, next) {
+    var id = req.params.id
+    db.collection('picture').deleteOne({
+        _id: mongo.ObjectID(id)
+    }, done)
+
+    function done(err) {
+        if (err) {
+            next(err)
+        } else {
+            res.json({
+                status: 'ok'
+            })
+        }
+    }
+}
+
+function form(req, res) {
+    res.render('upload.ejs')
 }
